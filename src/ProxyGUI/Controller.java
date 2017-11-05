e9cfec813d644ef5d8867a2ed7a02df129f1f941
Trent Killinger <trentkillinger@gmail.com>
2017-11-05 14:12:01 -0500

    Finished

--- src/ProxyGUI/Controller.java
+++ src/ProxyGUI/Controller.java
@@ -8,19 +8,31 @@ import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
 
+import javax.swing.text.FieldView;
 import javax.swing.text.Style;
 import java.io.*;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.URL;
+import java.nio.file.Files;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.ResourceBundle;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicLong;
 
-public class Controller implements Initializable {
+/**
+ * The controller that handles the events from the gui
+ *
+ * @author  Trent Killinger
+ * @version 1.0
+ * @since   9-27-2017
+ */
+
+public class Controller {
 
     @FXML
     private TextArea textCache;
@@ -48,105 +60,183 @@ public class Controller implements Initializable {
 
     @FXML
     private TextField textPort;
-
+    /*Manages threads*/
     private ExecutorService executor;
+    /*Socket Server*/
     private ServerSocket fromClient;
+    /*Thread ID tracker*/
     private final AtomicLong sequenceNumber = new AtomicLong(0);
 
-    @Override
-    public void initialize(URL locations, ResourceBundle resources) {
-        executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
-    }
-
+    /**
+     * Starts the server
+     * @param none
+     * @return Nothing
+     */
     public void StarServer() {
         buttonStart.setDisable(true);
         try {
             fromClient = new ServerSocket(Integer.parseInt(textPort.getText()));
-            executor.execute(() ->{
-                while(true) {
+            executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+            executor.execute(() -> {
+                while (true) {
                     try {
                         Socket request = fromClient.accept();
-                        HandleConnection(request,executor);
-                    }
-                    catch (Exception ex) {
-                        ResetServer();
+                        HandleConnection(request, executor);
+                    } catch (Exception ex) {
+
                     }
                 }
             });
 
             serverStatus.setFill(Color.GREEN);
-        }
-        catch (Exception ex) {
-            ResetServer();
+        } catch (Exception ex) {
             System.out.println(ex);
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Proxy has crashed.");
+            alert.setHeaderText("Your proxy has crashed");
+            alert.setContentText("Please close and reopen the program");
+            alert.show();
         }
     }
 
-    public void HandleConnection(Socket request,ExecutorService executor) {
+    /**
+     * Handles the individual connection
+     * @param request
+     * @param executor
+     * @return Nothing
+     */
+    public void HandleConnection(Socket request, ExecutorService executor) {
         executor.execute(() -> {
             try {
                 long ID = sequenceNumber.getAndIncrement();
 
                 HTTPObject clientHttpRequest = new HTTPObject(request.getInputStream());
 
-                String hostIP = InetAddress.getByName(new URL(("http://" + clientHttpRequest.HeaderParameter("Host").replace(" ",""))).getHost()).getHostAddress();
+                String host = clientHttpRequest.HeaderParameter("Host").replace(" ", "");
 
-                if(clientHttpRequest.Method().toUpperCase().equals("GET")) {
-                    Platform.runLater(new UpdateTextField(textClientRequests,"Handling a new request: " + ID + "\n" + Arrays.toString(clientHttpRequest.GetBytes()) + "\n\n"));
+                String hostIP = InetAddress.getByName(new URL(("http://" + host)).getHost()).getHostAddress();
 
-                    Socket toServer = new Socket(hostIP,80);
+                if (clientHttpRequest.Method().toUpperCase().equals("GET")) {
+                    Platform.runLater(new UpdateTextField(textClientRequests, "Handling a new request to " + host + " from client, Thread ID: " + ID + "\n"));
+
+                    Socket toServer = new Socket(hostIP, 80);
 
                     DataOutputStream outToServer = new DataOutputStream(toServer.getOutputStream());
 
-                    Platform.runLater(new UpdateTextField(textServerRequests,"Sending a new request: " + ID + "\n" + Arrays.toString(clientHttpRequest.GetBytes()) + "\n\n"));
+                    Platform.runLater(new UpdateTextField(textServerRequests, "Sending a new request to server IP " + hostIP + ", Thread ID: " + ID + "\n"));
+
+                    if (ObjectExistInCache(clientHttpRequest.Path().replace('.', '-').split("\\?")[0])) {
+                        clientHttpRequest.AddHeaderParameter("If-Modified-Since", CachedObjectLastModifiedScince(clientHttpRequest.Path().replace('.', '-').split("\\?")[0]));
+                    }
 
                     outToServer.write(clientHttpRequest.GetBytes());
 
                     HTTPObject serverHttpResponse = new HTTPObject(toServer.getInputStream());
 
-                    Platform.runLater(new UpdateTextField(textSeverResponses,"Receiving a new response: " + ID + "\n" + Arrays.toString(serverHttpResponse.GetBytes()) + "\n\n"));
+                    Platform.runLater(new UpdateTextField(textSeverResponses, "Receiving a new response from server, Thread ID: " + ID + "\n"));
 
-                    request.getOutputStream().write(serverHttpResponse.GetBytes());
+                    DataOutputStream outToClient = new DataOutputStream(request.getOutputStream());
 
-                    Platform.runLater(new UpdateTextField(textClientResponses,"Sending a new response: " + ID + "\n" + Arrays.toString(serverHttpResponse.GetBytes()) + "\n\n"));
+                    if (serverHttpResponse.GeneralHeader().equals("HTTP/1.1 304 Not Modified")) {
+                        serverHttpResponse.Body(GetObjectFromCache(clientHttpRequest.Path().replace('.', '-').split("\\?")[0]));
+                    } else {
+                        SaveObjectToCache(clientHttpRequest.Path().replace('.', '-').split("\\?")[0], serverHttpResponse.Body());
+                    }
+                    outToClient.write(serverHttpResponse.GetBytes());
 
+                    Platform.runLater(new UpdateTextField(textClientResponses, "Sending a new response to client, Thread ID " + ID + "\n"));
+
+                    outToServer.close();
                     toServer.close();
-                    fromClient.close();
+                    outToClient.close();
 
                 }
 
                 request.close();
 
             } catch (Exception ex) {
+
+
                 System.out.println(ex);
             }
         });
     }
 
-    private void SaveObject() {
-
-    }
-
-    private void GetObject() {
-
-    }
-
-    private void ResetServer() {
+    /**
+     * Saves the object to the cache
+     * @param path
+     * @param data
+     * @return Nothing
+     */
+    private void SaveObjectToCache(String path, byte[] data) {
         try {
-            buttonStart.setDisable(false);
-            serverStatus.setFill(Color.RED);
-            fromClient.close();
-        }catch (Exception ex) {
+
+            File cache = new File(System.getProperty("java.io.tmpdir") + "\\" + path);
+            cache.getParentFile().mkdirs();
+
+            FileOutputStream cacheWriter = new FileOutputStream(cache.getPath());
+            cacheWriter.write(data);
+            cacheWriter.close();
+
+            Platform.runLater(new UpdateTextField(textCache, "Saving file " + path + " to cache\n"));
+        } catch (Exception ex) {
+            System.out.println(ex);
+        } finally {
 
         }
+
     }
 
+    /**
+     * Formats the cached file date
+     * @param path
+     * @return string
+     */
+    private String CachedObjectLastModifiedScince(String path) {
+        File cache = new File(System.getProperty("java.io.tmpdir") + "\\" + path);
+        SimpleDateFormat outputFormat = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z");
+        Date lastModified = new Date(cache.lastModified());
+        return outputFormat.format(lastModified);
+    }
+
+    /**
+     * Determines if the cached file exists
+     * @param path
+     * @return boolean
+     */
+    private boolean ObjectExistInCache(String path) {
+        File cache = new File(System.getProperty("java.io.tmpdir") + "\\" + path);
+        return cache.exists();
+    }
+
+    /**
+     * Gets the object form the cache
+     * @param path
+     * @return byte[]
+     */
+    private byte[] GetObjectFromCache(String path) {
+        try {
+            File cache = new File(System.getProperty("java.io.tmpdir") + "\\" + path);
+            Platform.runLater(new UpdateTextField(textCache, "Loading file " + path + " from cache\n"));
+            return Files.readAllBytes(cache.toPath());
+        } catch (Exception ex) {
+            System.out.println(ex);
+        } finally {
+
+        }
+        return new byte[0];
+    }
+
+    /**
+     * Closes the TCP server
+     * @param none
+     * @return Nothing
+     */
     public void OnClose() {
         try {
             fromClient.close();
             System.out.println("Closed");
-        }
-        catch (Exception ex) {
+        } catch (Exception ex) {
 
         }
     }
--- src/ProxyGUI/HTTPObject.java
+++ src/ProxyGUI/HTTPObject.java
@@ -3,23 +3,26 @@ package ProxyGUI;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.Map;
 
 /**
- * The HTTPObject stores the request
+ * The HTTPObject stores the request/result
  *
  * @author  Trent Killinger
  * @version 1.0
- * @since   9-22-2017
+ * @since   9-27-2017
  */
 public class HTTPObject {
     /*General Header*/
     private String generalHeader;
     /*Collection of headers*/
     private Hashtable<String,String> requestHeaders;
-    /*Request Body*/
+    /*Body*/
     private byte[] body;
+    /*Header Seciont*/
     private String headerSection;
-
+    /*Stream That reads request/response*/
+    private BufferedReader readStream;
     /**
      * Class constructor
      * @param requestText request text.
@@ -34,10 +37,10 @@ public class HTTPObject {
         requestHeaders = new Hashtable<>();
         body = new byte[0];
 
-        BufferedReader httpRequestStream = new BufferedReader(new InputStreamReader(stream));
+        BufferedReader readStream = new BufferedReader(new InputStreamReader(stream));
 
         String line;
-        while((line = httpRequestStream.readLine()) != null) {
+        while((line = readStream.readLine()) != null) {
             if(line.length() == 0) {
                 break;
             }
@@ -51,17 +54,19 @@ public class HTTPObject {
 
         String header = reader.readLine();
 
-        while (header.length() > 0 ) {
-            int index = header.indexOf(":");
-            if (index == -1) {
-                throw new Exception("Header parameter is invalid: " + header);
+        if(header != null) {
+            while (header.length() > 0 ) {
+                int index = header.indexOf(":");
+                if (index != -1) {
+                    requestHeaders.put(header.substring(0, index), header.substring(index + 1, header.length()));
+                    header = reader.readLine();
+                } else {
+                    System.out.println("Header parameter is invalid: " + header);
+                }
             }
-            requestHeaders.put(header.substring(0, index), header.substring(index + 1, header.length()));
-            header = reader.readLine();
         }
 
         try {
-            ByteArrayOutputStream bodyBytes = new ByteArrayOutputStream();
             boolean found = false;
             for (String key : requestHeaders.keySet()) {
                 if(key.equals("Content-Length")) {
@@ -72,18 +77,15 @@ public class HTTPObject {
             if(found) {
                 body = new byte[Integer.parseInt(HeaderParameter("Content-Length").trim())];
                 for(int i = 0; i < Integer.parseInt(HeaderParameter("Content-Length").trim());i++) {
-
+                    int readInt = readStream.read();
+                    if(readInt != -1) {
+                        body[i] = (byte) readInt;
+                    }
                 }
+                System.out.println("All bytes have been added to the array");
             }
-            else {
-
-                int read = httpRequestStream.read();
-                while (read != -1) {
-                    bodyBytes.write(read);
-                    read = httpRequestStream.read();
-                }
-            }
-            body = bodyBytes.toByteArray();
+        } catch (IOException ex) {
+            System.out.println(ex);
         } catch (Exception ex) {
             System.out.println(ex);
         }
@@ -109,6 +111,16 @@ public class HTTPObject {
     }
 
     /**
+     * Adds header to request/response
+     * @param header
+     * @param value
+     * @return none.
+     */
+    public void AddHeaderParameter(String header,String value) {
+        requestHeaders.put(header,value);
+    }
+    
+    /**
      * Gets Body
      * @param none
      * @return byte[]
@@ -117,15 +129,74 @@ public class HTTPObject {
         return body;
     }
 
+    /**
+     * Sets Body
+     * @param body
+     * @return none
+     */
+    public void Body(byte[] body) {
+        body = this.body;
+    }
+
+    /**
+     * Gets Request/Response Method
+     * @param none
+     * @return String
+     */
     public String Method() {return generalHeader.split(" ")[0];}
 
+    /**
+     * Gets Request/Response Path
+     * @param none
+     * @return String
+     */
+    public String Path() {return generalHeader.split(" ")[1].replace("http://","");}
+
+    /**
+     * Gets Request/Response URI
+     * @param none
+     * @return String
+     */
     public String URI() {return  generalHeader.split(" ")[1];}
 
-    public byte[] GetBytes() {
-        byte[] combinedHeaderandBody = new byte[headerSection.getBytes().length + body.length];
+    /**
+     * Attempts to close the stream
+     * @param none
+     * @return Boolean
+     */
+    public Boolean CloseStream() {
+        try {
+            this.readStream.close();
+            return true;
+        } catch (Exception ex){
+            return false;
+        }
 
-        System.arraycopy(headerSection.getBytes(),0,combinedHeaderandBody,0,headerSection.getBytes().length);
-        System.arraycopy(body,0,combinedHeaderandBody,headerSection.getBytes().length,body.length);
+    }
+
+    /**
+     * Formats the headers into a string from the header dictionary
+     * @param none
+     * @return String
+     */
+    private String FormatHeaders() {
+        String formatedString = generalHeader + "\r\n";
+        for (Map.Entry<String,String> entry : requestHeaders.entrySet()) {
+            formatedString += entry.getKey() + ": " + entry.getValue() + "\r\n";
+        }
+        return formatedString + "\r\n";
+    }
+
+    /**
+     * Gets the byte representaion from the request/response
+     * @param none
+     * @return byte[]
+     */
+    public byte[] GetBytes() {
+        String headers = FormatHeaders();
+        byte[] combinedHeaderandBody = new byte[headers.getBytes().length + body.length];
+        System.arraycopy(headers.getBytes(),0,combinedHeaderandBody,0,headers.getBytes().length);
+        System.arraycopy(body,0,combinedHeaderandBody,headers.getBytes().length,body.length);
 
         return combinedHeaderandBody;
     }
